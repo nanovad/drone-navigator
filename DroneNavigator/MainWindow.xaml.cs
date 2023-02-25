5cb8e323eee2032fba60daf4fb09266d76ed2b54
nanovad <nanovad@gmail.com>
2023-02-25 11:40:31 -0600

    Implement the splash screen UI
    Implement the splash screen UI with a list of previous missions, as well as a
    button to start a new mission (which creates a dialog box). Actually launching
    the Flight interface will be implemented later.

--- DroneNavigator/MainWindow.xaml.cs
+++ DroneNavigator/MainWindow.xaml.cs
@@ -15,6 +15,9 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using FlightDataModel;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging.Abstractions;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -26,14 +29,50 @@ namespace DroneNavigator
     /// </summary>
     public sealed partial class MainWindow : Window
     {
+        private FlightDataContext fdc;
+
         public MainWindow()
         {
             this.InitializeComponent();
+            Microsoft.UI.Dispatching.
+                DispatcherQueue.GetForCurrentThread().TryEnqueue(Loaded);
+            fdc = new FlightDataContext();
+            fdc.Database.Migrate();
         }
 
-        private void myButton_Click(object sender, RoutedEventArgs e)
+        public void Loaded()
         {
-            myButton.Content = "Clicked";
+            List<MissionModel> sourceList = fdc.Missions.ToList();
+            MissionListView.ItemsSource = sourceList;
+        }
+
+        private void ReplayMissionButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            FrameworkElement el = (FrameworkElement)sender;
+            MissionModel selectedMission = (MissionModel)el.DataContext;
+        }
+
+        private void MissionListView_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // Disable selection by immediately deselecting upon selecting.
+            MissionListView.SelectedItem = null;
+        }
+
+        private async void StartNewMissionButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            // Spawn the "Start new mission" dialog.
+            ContentDialog cd = new();
+            cd.XamlRoot = this.Content.XamlRoot;
+            cd.Content = new StartNewMissionDialog();
+            cd.PrimaryButtonText = "Start";
+            cd.CloseButtonText = "Cancel";
+            ContentDialogResult result = await cd.ShowAsync();
+
+            if (result == ContentDialogResult.Primary)
+            {
+                // Now cast (cd.Content as StartNewMissionDialog).<field> to access the fields from the dialog.
+                // Save the mission to the DB and spawn a new Flight window that will target the newly created mission.
+            }
         }
     }
 }
