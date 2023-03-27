nanovad/drone-navigator

commit 0f5e2d4eb7a4c643517f7422986a8d60fc8c07d9

Comprehensive UI rework
Convert the MainWindow of DroneNavigator (i.e. the app's landing page) to use a
NavigationView as its primary control. The NavigationView contains two pages:
1. MissionListPage
2. DroneListPage

MissionListPage is the default page, and contains a list of previously flown
missions, which is the same as the previous landing screen. The search bar (not
yet functional) and "Start new mission" button remain unchanged at the bottom of
the list. The margin between list items has been increased to 4. Each list item
now also shows the name and description of the mission. The expander for each
list item also now shows the date and time that the mission started, as well as
the mission duration.

DroneListPage is an entirely new page for managing drones. Drones can be
created, edited, and deleted from this page. DroneListPage is structured
similarly to MisisonListPage; a list of drones is the main focus of the page,
along with a button to create a new drone at the bottom of the page, which
launches a ContentDialog. Each drone in the list has buttons to delete (which
currently violates referential integrity until the data model is updated to
allow for "disabling" of old drones when the user chooses to delete them), or
edit, which is a safe operation. Each list item shows information about the
drone, such as the name, make, model, mission count, and total flight time.

DroneNavigator/MissionListPage.xaml.cs
@@ -1,0 +1,118 @@
+// Copyright (c) Microsoft Corporation and Contributors.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using FlightDataModel;
+using Microsoft.EntityFrameworkCore;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace DroneNavigator
+{
+    public sealed partial class MissionListPage : Page
+    {
+
+        private readonly FlightDataContext fdc;
+
+        public MissionListPage()
+        {
+            this.InitializeComponent();
+            Microsoft.UI.Dispatching.
+                DispatcherQueue.GetForCurrentThread().TryEnqueue(Loaded);
+            fdc = new FlightDataContext();
+            fdc.Database.Migrate();
+        }
+
+        public async void Loaded()
+        {
+            List<MissionModel> sourceList = await fdc.Missions.ToListAsync();
+            MissionListView.ItemsSource = sourceList.OrderByDescending(mission => mission.StartDateTimeOffset);
+        }
+
+        static internal string FormatMissionDuration(TimeSpan missionDuration)
+        {
+            return missionDuration.ToString("hh\\:mm\\:ss\\.fff");
+        }
+
+        private void ReplayMissionButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            FrameworkElement el = (FrameworkElement)sender;
+            MissionModel selectedMission = (MissionModel)el.DataContext;
+            Review.MainWindow reviewWindow = new();
+            reviewWindow.Initialize(selectedMission);
+            reviewWindow.Activate();
+        }
+
+        private void MissionListView_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // Disable selection by immediately deselecting upon selecting.
+            MissionListView.SelectedItem = null;
+        }
+
+        private async void StartNewMissionButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            // Spawn the "Start new mission" dialog.
+            StartNewMissionDialog snmd = new();
+            ContentDialog cd = new();
+            cd.XamlRoot = this.Content.XamlRoot;
+            cd.Content = snmd;
+            cd.PrimaryButtonText = "Start";
+            cd.CloseButtonText = "Cancel";
+            cd.PrimaryButtonClick += delegate(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+            {
+                // TODO: Validate snmd.DroneId?
+                if(String.IsNullOrEmpty(snmd.MissionName)) {
+                    snmd.ErrorMessage = "Must enter a mission name";
+                    args.Cancel = true;
+                }
+                else if(String.IsNullOrEmpty(snmd.MissionDescription)) {
+                    snmd.ErrorMessage = "Must enter a mission description";
+                    args.Cancel = true;
+                }
+            };
+            ContentDialogResult result = await cd.ShowAsync();
+
+            if (result == ContentDialogResult.Primary)
+            {
+                // Commit the mission (as defined by the dialog's textboxes) to the DB
+                MissionModel newMission = new()
+                {
+                    Drone = snmd.DroneId ?? 0, // BUG: Can't have a 0 DroneId, probably a recipe for breakage
+                    Name = snmd.MissionName,
+                    Description = snmd.MissionDescription,
+                    StartDateTimeOffset = DateTimeOffset.Now
+                };
+                fdc.Missions.Add(newMission);
+                fdc.SaveChanges();
+
+                // EF Core will have generated an ID for newMission at this point, so we can use it here.
+                // Begin initializing a new Flight window
+                Flight.MainWindow flightWindow = new();
+                flightWindow.Mission = newMission;
+                // Close our window - we're about to swap to the Flight interface
+                //this.Close();
+
+                flightWindow.Activate();
+                flightWindow.OnMissionVideoEncodingCompleted += delegate
+                {
+                    MainWindow newMain = new();
+                    newMain.Activate();
+                };
+            }
+        }
+    }
+}

DroneNavigator/MainWindow.xaml.cs
@@ -29,87 +29,26 @@ namespace DroneNavigator
     /// </summary>
     public sealed partial class MainWindow : Window
     {
-        private FlightDataContext fdc;
-
         public MainWindow()
         {
             this.InitializeComponent();
-            Microsoft.UI.Dispatching.
-                DispatcherQueue.GetForCurrentThread().TryEnqueue(Loaded);
-            fdc = new FlightDataContext();
-            fdc.Database.Migrate();
+            MainNavigationView.SelectedItem = MainNavigationView.MenuItems.ElementAt(0);
         }
 
-        public void Loaded()
+        private void MainNavigationView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
         {
-            List<MissionModel> sourceList = fdc.Missions.ToList();
-            MissionListView.ItemsSource = sourceList;
-        }
-
-        private void ReplayMissionButton_OnClick(object sender, RoutedEventArgs e)
-        {
-            FrameworkElement el = (FrameworkElement)sender;
-            MissionModel selectedMission = (MissionModel)el.DataContext;
-            Review.MainWindow reviewWindow = new();
-            reviewWindow.Initialize(selectedMission);
-            reviewWindow.Activate();
-        }
-
-        private void MissionListView_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            // Disable selection by immediately deselecting upon selecting.
-            MissionListView.SelectedItem = null;
-        }
-
-        private async void StartNewMissionButton_OnClick(object sender, RoutedEventArgs e)
-        {
-            // Spawn the "Start new mission" dialog.
-            StartNewMissionDialog snmd = new();
-            ContentDialog cd = new();
-            cd.XamlRoot = this.Content.XamlRoot;
-            cd.Content = snmd;
-            cd.PrimaryButtonText = "Start";
-            cd.CloseButtonText = "Cancel";
-            cd.PrimaryButtonClick += delegate(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+            if(args.IsSettingsSelected)
             {
-                // TODO: Validate snmd.DroneId?
-                if(String.IsNullOrEmpty(snmd.MissionName)) {
-                    snmd.ErrorMessage = "Must enter a mission name";
-                    args.Cancel = true;
-                }
-                else if(String.IsNullOrEmpty(snmd.MissionDescription)) {
-                    snmd.ErrorMessage = "Must enter a mission description";
-                    args.Cancel = true;
-                }
-            };
-            ContentDialogResult result = await cd.ShowAsync();
-
-            if (result == ContentDialogResult.Primary)
+                // We do not have a settings page - short circuit
+                return;
+            }
+            NavigationViewItem selected = (NavigationViewItem)args.SelectedItem;
+            if(args.SelectedItem != null)
             {
-                // Commit the mission (as defined by the dialog's textboxes) to the DB
-                MissionModel newMission = new()
-                {
-                    Drone = snmd.DroneId ?? 0, // BUG: Can't have a 0 DroneId, probably a recipe for breakage
-                    Name = snmd.MissionName,
-                    Description = snmd.MissionDescription,
-                    StartDateTimeOffset = DateTimeOffset.Now
-                };
-                fdc.Missions.Add(newMission);
-                fdc.SaveChanges();
-
-                // EF Core will have generated an ID for newMission at this point, so we can use it here.
-                // Begin initializing a new Flight window
-                Flight.MainWindow flightWindow = new();
-                flightWindow.Mission = newMission;
-                // Close our window - we're about to swap to the Flight interface
-                this.Close();
-
-                flightWindow.Activate();
-                flightWindow.OnMissionVideoEncodingCompleted += delegate
-                {
-                    MainWindow newMain = new();
-                    newMain.Activate();
-                };
+                string tag = selected.Tag as string;
+                string pageName = "DroneNavigator." + tag;
+                Type pageType = Type.GetType(pageName);
+                MainNavigationFrame.Navigate(pageType);
             }
         }
     }

DroneNavigator/DroneListPage.xaml
@@ -1,0 +1,66 @@
+<!-- Copyright (c) Microsoft Corporation and Contributors. -->
+<!-- Licensed under the MIT License. -->
+
+<Page
+    x:Class="DroneNavigator.DroneListPage"
+    xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
+    xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
+    xmlns:local="using:DroneNavigator"
+    xmlns:d="http://schemas.microsoft.com/expression/blend/2008"
+    xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006"
+    mc:Ignorable="d"
+    xmlns:flightDataModel="using:FlightDataModel"
+    Loaded="Page_Loaded"
+    Background="{ThemeResource ApplicationPageBackgroundThemeBrush}">
+
+    <Grid>
+        <Grid.RowDefinitions>
+            <RowDefinition Height="*" />
+            <RowDefinition Height="Auto" />
+        </Grid.RowDefinitions>
+        <ListView x:Name="DronesListView" Grid.Row="0">
+            <ListView.ItemTemplate>
+                <DataTemplate x:DataType="flightDataModel:DroneModel" xmlns:xaml="http://schemas.microsoft.com/winfx/2006/xaml/presentation">
+                    <Grid Background="{xaml:ThemeResource SystemColorWindowColor}">
+                        <Grid.ColumnDefinitions>
+                            <ColumnDefinition Width="*" />
+                            <ColumnDefinition Width="Auto" />
+                            <ColumnDefinition Width="Auto" />
+                        </Grid.ColumnDefinitions>
+                        <RichTextBlock Grid.Column="0" IsTextSelectionEnabled="False" Margin="16">
+                            <Paragraph>
+                                <Run Text="{x:Bind Name}" FontSize="20" FontWeight="Bold" />
+                                <Run Text=" - " />
+                                <Run Text="{x:Bind Make}" FontSize="16" FontStyle="Italic" />
+                                <Run Text="{x:Bind Model}" FontSize="16" FontStyle="Italic" />
+                            </Paragraph>
+                            <Paragraph FontWeight="SemiBold" Margin="16,0,0,0">
+                                <Run Text="Cumulative flight duration: " />
+                                <Run Text="{x:Bind TotalFlightTime}" FontWeight="Normal"/>
+                                <LineBreak />
+                                <Run Text="Mission count: " />
+                                <Run Text="{x:Bind MissionCount}" />
+                            </Paragraph>
+                        </RichTextBlock>
+                        <Button x:Name="EditDroneButton" Grid.Column="1" Margin="16,0,8,0" Click="EditDroneButton_Click">
+                            <SymbolIcon Symbol="Edit" />
+                        </Button>
+                        <Button x:Name="DeleteDroneButton" Grid.Column="2" Margin="8,0,32,0" Click="DeleteDroneButton_Click">
+                            <SymbolIcon Symbol="Delete" />
+                        </Button>
+                    </Grid>
+                </DataTemplate>
+            </ListView.ItemTemplate>
+        </ListView>
+        <Border Grid.Row="1" BorderBrush="Silver" CornerRadius="8" Padding="5" BorderThickness="1" Margin="10">
+            <RelativePanel x:Name="CommandButtonRow" Margin="8">
+                <Button x:Name="NewDroneButton" RelativePanel.AlignRightWithPanel="True" Click="NewDroneButton_Click">
+                    <StackPanel Orientation="Horizontal">
+                        <SymbolIcon Symbol="Add" Margin="0,0,8,0" VerticalAlignment="Center" />
+                        <TextBlock VerticalAlignment="Center">Add Drone</TextBlock>
+                    </StackPanel>
+                </Button>
+            </RelativePanel>
+        </Border>
+    </Grid>
+</Page>

DroneNavigator/DroneListPage.xaml.cs
@@ -1,0 +1,95 @@
+// Copyright (c) Microsoft Corporation and Contributors.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using FlightDataModel;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.UI.Dispatching;
+using Windows.Services.Maps;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace DroneNavigator
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class DroneListPage : Page
+    {
+        FlightDataContext fdc = new();
+
+        DispatcherQueue _dq = DispatcherQueue.GetForCurrentThread();
+
+        public DroneListPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            RefreshList();
+        }
+
+        private void RefreshList()
+        {
+            fdc.Drones.ToListAsync().ContinueWith(
+                (items) => _dq.TryEnqueue(
+                    () => DronesListView.ItemsSource = items.Result));
+        }
+
+        private async void NewDroneButton_Click(object sender, RoutedEventArgs e)
+        {
+            EditDroneDialog edd = new();
+            edd.XamlRoot = this.Content.XamlRoot;
+            if(await edd.ShowAsync() == ContentDialogResult.Primary)
+                RefreshList();
+        }
+
+        private async void EditDroneButton_Click(object sender, RoutedEventArgs e)
+        {
+            DroneModel whichDrone = (e.OriginalSource as Button).DataContext as DroneModel;
+
+            EditDroneDialog edd = new();
+            edd.XamlRoot = this.Content.XamlRoot;
+            edd.Prefill = whichDrone;
+            if(await edd.ShowAsync() == ContentDialogResult.Primary)
+                RefreshList();
+        }
+
+        private async void DeleteDroneButton_Click(object sender, RoutedEventArgs e)
+        {
+            DroneModel whichDrone = (e.OriginalSource as Button).DataContext as DroneModel;
+
+            ContentDialog cd = new();
+            cd.XamlRoot = this.Content.XamlRoot;
+            cd.Title = "Delete drone?";
+            cd.Content = $"Are you sure you want to delete the drone \"{whichDrone.Name}\"?";
+            cd.PrimaryButtonText = "Delete";
+            cd.SecondaryButtonText = "Cancel";
+
+            ContentDialogResult res = await cd.ShowAsync();
+
+            if(res == ContentDialogResult.Primary) {
+                // TODO: We cannot simply remove the drone, as this will violate referential integrity.
+                // We have to add a parameter to it to mark it as deleted.
+                fdc.Remove(whichDrone);
+                fdc.SaveChanges();
+                RefreshList();
+            }
+        }
+    }
+}

DroneNavigator/DroneNavigator.csproj
@@ -15,6 +15,9 @@
     <Nullable>enable</Nullable>
   </PropertyGroup>
   <ItemGroup>
+    <None Remove="DroneListPage.xaml" />
+    <None Remove="EditDroneDialog.xaml" />
+    <None Remove="MissionListPage.xaml" />
     <None Remove="StartNewMissionDialog.xaml" />
   </ItemGroup>
 
@@ -52,6 +55,21 @@
   </ItemGroup>
   <ItemGroup>
     <Folder Include="Themes\" />
+  </ItemGroup>
+  <ItemGroup>
+    <Page Update="EditDroneDialog.xaml">
+      <Generator>MSBuild:Compile</Generator>
+    </Page>
+  </ItemGroup>
+  <ItemGroup>
+    <Page Update="DroneListPage.xaml">
+      <Generator>MSBuild:Compile</Generator>
+    </Page>
+  </ItemGroup>
+  <ItemGroup>
+    <Page Update="MissionListPage.xaml">
+      <Generator>MSBuild:Compile</Generator>
+    </Page>
   </ItemGroup>
   <ItemGroup>
     <Page Update="StartNewMissionDialog.xaml">

DroneNavigator/EditDroneDialog.xaml
@@ -1,0 +1,37 @@
+<!-- Copyright (c) Microsoft Corporation and Contributors. -->
+<!-- Licensed under the MIT License. -->
+
+<!-- Need to manually set the style of subclassed ContentDialogs
+     See https://github.com/microsoft/microsoft-ui-xaml/issues/3486 -->
+<ContentDialog
+    x:Class="DroneNavigator.EditDroneDialog"
+    xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
+    xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
+    xmlns:local="using:DroneNavigator"
+    xmlns:d="http://schemas.microsoft.com/expression/blend/2008"
+    xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006"
+    mc:Ignorable="d"
+    Title="Create a new drone"
+    PrimaryButtonText="Save"
+    SecondaryButtonText="Cancel"
+    DefaultButton="Primary"
+    Style="{StaticResource DefaultContentDialogStyle}"
+    PrimaryButtonClick="NewDroneDialog_PrimaryButtonClick">
+
+    <Page Background="Transparent">
+        <Grid>
+            <Grid.RowDefinitions>
+                <RowDefinition Height="Auto" />
+                <RowDefinition Height="Auto" />
+                <RowDefinition Height="Auto" />
+                <RowDefinition Height="Auto" />
+            </Grid.RowDefinitions>
+            <TextBox x:Name="DroneNameTextBox" Grid.Row="0" Margin="8" PlaceholderText="Name" />
+            <TextBox x:Name="DroneMakeTextBox" Grid.Row="1" Margin="8" PlaceholderText="Make" />
+            <TextBox x:Name="DroneModelTextBox" Grid.Row="2" Margin="8" PlaceholderText="Model" />
+
+            <TextBlock x:Name="ValidationMessageTextBox" Grid.Row="3"
+                   Foreground="Red" FontWeight="SemiBold" Visibility="Collapsed">Unable to save.</TextBlock>
+        </Grid>
+    </Page>
+</ContentDialog>

DroneNavigator/EditDroneDialog.xaml.cs
@@ -1,0 +1,82 @@
+// Copyright (c) Microsoft Corporation and Contributors.
+// Licensed under the MIT License.
+
+using FlightDataModel;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace DroneNavigator
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class EditDroneDialog : ContentDialog
+    {
+        public DroneModel? Prefill { get; set; }
+
+        public EditDroneDialog() : base()
+        {
+            this.InitializeComponent();
+        }
+
+        public new IAsyncOperation<ContentDialogResult> ShowAsync()
+        {
+            // TODO: Should we use DataContext?
+            if(Prefill is not null) {
+                DroneNameTextBox.Text = Prefill!.Name;
+                DroneMakeTextBox.Text = Prefill!.Make;
+                DroneModelTextBox.Text = Prefill!.Model;
+            }
+            return base.ShowAsync();
+        }
+
+        private void NewDroneDialog_PrimaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        {
+            List<string> errors = new();
+            if(string.IsNullOrWhiteSpace(DroneNameTextBox.Text))
+                errors.Add("drone name cannot be empty or only whitespace");
+            if(string.IsNullOrWhiteSpace(DroneMakeTextBox.Text))
+                errors.Add("drone make cannot be empty or only whitespace");
+            if(string.IsNullOrWhiteSpace(DroneModelTextBox.Text))
+                errors.Add("drone model cannot be empty or only whitespace");
+
+            if(errors.Count == 0)
+            {
+                DroneModel? drone = Prefill switch {
+                    null => new(),
+                    _ => Prefill
+                };
+                drone.Name = DroneNameTextBox.Text;
+                drone.Make = DroneMakeTextBox.Text;
+                drone.Model = DroneModelTextBox.Text;
+
+                FlightDataContext fdc = new();
+                fdc.Update(drone);
+                fdc.SaveChanges();
+                return;
+            }
+
+            // Otherwise we have an error
+            // Capitalize the first letter for aesthetics
+            errors[0] = char.ToUpper(errors[0][0]) + errors[0][1..];
+            ValidationMessageTextBox.Text = string.Join(", ", errors);
+            ValidationMessageTextBox.Visibility = Visibility.Visible;
+            args.Cancel = true;
+        }
+    }
+}

commit c453ed4fb0b11a0b4a54e0f80f52991a3cb0e6bf

Close MainWindow during Flight
Fixes a regression from 0f5e2d4eb7a4c643517f7422986a8d60fc8c07d9. Prior to that
commit, the MainWindow would close when a new mission was successfully started.
The commit commented out that behavior because there was not an existing way to
get a reference to the MainWindow that was the parent of the NavigationFrame in
the new UI structure.

Fix the aforementioned regression by exposing a reference to the active main
window from DroneNavigator.App, referencing that via the Application.Current
helper, and calling its Close method.

Fix another bug where the main window would not reopen after flight if no video
was encoded. This bug occurred because the callback to reinstantiate the
MainWindow was hooked to the OnMissionVideoEncodingCompleted event handler,
which would not fire when no encoding was required (mainly occurring when no
mission was flown / the drone was never connected to). The fix is to simply add
a new FlightFinished event handler to the Flight window class that is fired
when the window is closing, either after encoding finishes or when no encoding
is required. The regular OnClosed event cannot be hooked since it is intercepted
to trigger encoding.

DroneNavigator/App.xaml.cs
@@ -51,5 +51,6 @@ namespace DroneNavigator
         }
 
         private Window m_window;
+        public Window Window => m_window;
     }
 }

DroneNavigator/MissionListPage.xaml.cs
@@ -109,10 +109,10 @@ namespace DroneNavigator
                 Flight.MainWindow flightWindow = new();
                 flightWindow.Mission = newMission;
                 // Close our window - we're about to swap to the Flight interface
-                //this.Close();
+                (Application.Current as App)?.Window.Close();
 
                 flightWindow.Activate();
-                flightWindow.OnMissionVideoEncodingCompleted += delegate
+                flightWindow.FlightFinished += delegate
                 {
                     MainWindow newMain = new();
                     newMain.Activate();

Flight/MainWindow.xaml.cs
@@ -52,6 +52,7 @@ namespace Flight
         private bool _videoBegan = false;
 
         public event EventHandler? OnMissionVideoEncodingCompleted;
+        public event EventHandler? FlightFinished;
 
         public MainWindow()
         {
@@ -204,8 +205,11 @@ namespace Flight
             // Short circuit if we've already encoded the video. This handler actually gets called twice - once when
             // the user initiates the window closing, and again when the OnEncodeCompleted callback is run.
             // This boolean ensures that we actually close the window the second time around.
-            if(_missionVideoEncoded)
+            if (_missionVideoEncoded)
+            {
+                FlightFinished?.Invoke(this, EventArgs.Empty);
                 return;
+            }
 
             var queue = new ConcurrentQueue<MediaStreamSample>();
 
@@ -219,7 +223,10 @@ namespace Flight
 
             // Track if any video was actually recorded - if not, there is nothing to encode.
             if (!_missionVideoToEncode)
+            {
+                FlightFinished?.Invoke(this, EventArgs.Empty);
                 return;
+            }
 
             // Save any changes (probably FlightState) to the database.
             _fdc.SaveChanges();
@@ -254,6 +261,7 @@ namespace Flight
                 api.VideoReceiver.VideoEncodingProperties,
                 MissionVideoManager.GetMissionVideoPath(Mission!));
             await mepd.ShowAsync();
+            FlightFinished?.Invoke(this, EventArgs.Empty);
         }
     }
 }